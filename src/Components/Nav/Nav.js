50b27e12052b8b2234e30b7e0cb36fb2c025ebe2
Cobrettie <cobrettiegarner3@gmail.com>
2020-03-04 19:13:35 -0500

    add first styled component to ../Nav.js

--- src/Components/Nav/Nav.js
+++ src/Components/Nav/Nav.js
@@ -2,21 +2,30 @@ import React from 'react';
 import { useRoutes, A } from 'hookrouter';
 import routes from '../../routes';
 import './Nav.css';
+import styled from 'styled-components';
+
+const MainNav = styled.nav`
+  border-bottom: 1px solid #373737;
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  margin: 0 auto;
+`;
 
 function Nav() {
   const routeResult = useRoutes(routes)
   return (
 
     <div>
-      <nav>
+      <MainNav>
         <div>
           <h1><A href='/'>Astronomy Photo of the Day</A></h1>
         </div>
 
         <div>
-          <h2><A href='/MarsRover'>Mars Rover Photos</A></h2>
+          <h2><A href='/MarsRover'>Mars Rover Latest Photo</A></h2>
         </div>
-      </nav>
+      </MainNav>
       {routeResult}
     </div>
   )
